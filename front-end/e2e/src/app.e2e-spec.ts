a06c1c472e517ca52fac82bd615469b493d0fe93
Florent <55687537+Frodon001@users.noreply.github.com>
2020-05-05 12:31:08 +0200

    Update app.e2e-spec.ts

--- front-end/e2e/src/app.e2e-spec.ts
+++ front-end/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {browser,logging} from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -16,3 +16,4 @@ describe('workspace-project App', () => {
     } as logging.Entry));
   });
 });
+
