3d08836658250120f49aa921558b976a8d09344a
VictoireMichel <56120737+VictoireMichel@users.noreply.github.com>
2020-05-19 20:39:04 +0200

    token ok

--- front-end/src/app/produit/produits.service.ts
+++ front-end/src/app/produit/produits.service.ts
@@ -11,7 +11,7 @@ export class ProduitsService {
   constructor(private http: HttpClient, private router: Router) {
   }
 
-  url = 'https://idlunch.wt1-2.ephec-ti.be:3000';
+  url = 'http://localhost:3000';
 
   getProduits(data) {
     return this
